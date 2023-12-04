a1acd4a6e4e9d3d38d1f9162f1e884c58bd2e9fe
chaitanyagoswami <chaitanya.goswami96@gmail.com>
2023-12-04 16:32:14 -0500

    Update run_non-invasive_sim.sh

--- Sec2-4/run_non-invasive_sim.sh
+++ Sec2-4/run_non-invasive_sim.sh
@@ -2,3 +2,6 @@
 python3 non-invasive_sim.py 0 100 1000
 python3 non-invasive_sim.py 1 100 1000
 python3 non-invasive_sim.py 2 100 1000
+python3 plot_summary_non-invasive.py 0
+python3 plot_summary_non-invasive.py 1
+python3 plot_summary_non-invasive.py 2
