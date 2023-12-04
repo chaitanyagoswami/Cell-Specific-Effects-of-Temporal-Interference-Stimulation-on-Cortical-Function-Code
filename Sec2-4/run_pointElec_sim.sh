74a358cca55a994e80b65bbb0187ffa868c0bc00
chaitanyagoswami <chaitanya.goswami96@gmail.com>
2023-12-04 16:31:17 -0500

    Update run_pointElec_sim.sh

--- Sec2-4/run_pointElec_sim.sh
+++ Sec2-4/run_pointElec_sim.sh
@@ -6,4 +6,5 @@ python3 pointElec_sim.py 4 6000 30000 6000 17000
 python3 pointElec_sim.py 8 10000 170000 10000 80000 
 python3 pointElec_sim.py 16 100000 500000 100000 300000
 python3 uniform_sim.py 0 300 0 300
+python3 plot_summary_pointElec.py
 
