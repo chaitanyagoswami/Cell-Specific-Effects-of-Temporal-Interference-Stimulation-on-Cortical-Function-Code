58f32eb21370344ddedc67cd88db2e2e3883b9fd
chaitanyagoswami <chaitanya.goswami96@gmail.com>
2024-07-19 11:57:40 -0400

    Update helper.py

--- CompStudy1-3/helper.py
+++ CompStudy1-3/helper.py
@@ -1,1 +1,36 @@
+import numpy as np
+import math
 
+### Helper Functions
+##################################################################################
+##################################################################################
+##################################################################################
+def cart_to_sph(pos):
+    if len(pos.shape) == 1:
+        pos = pos.reshape(1,-1)
+    r = np.sqrt(np.sum(pos**2, axis=1)).reshape(-1,1)
+    theta = np.arcsin(pos[:,2].reshape(-1,1)/r).reshape(-1,1)
+    phi = np.arctan2(pos[:,1],pos[:,0]).reshape(-1,1)
+    sph_pos = np.hstack([r,theta,phi])
+    return sph_pos
+    
+def sph_to_cart(pos):
+    if len(pos.shape) == 1:
+        pos = pos.reshape(1,-1)
+    x = pos[:,0]*np.cos(pos[:,1])*np.cos(pos[:,2])
+    y = pos[:,0]*np.cos(pos[:,1])*np.sin(pos[:,2])
+    z = pos[:,0]*np.sin(pos[:,1])
+    cart_pos = np.hstack([x.reshape(-1,1), y.reshape(-1,1), z.reshape(-1,1)])
+    return cart_pos
+
+def fibonacci_sphere(samples=1000):
+    points = []
+    phi = math.pi*(math.sqrt(5.)-1.)  # golden angle in radians
+    for i in range(samples):
+        y = 1-(i/float(samples-1))*2  # y goes from 1 to -1
+        radius = math.sqrt(1-y*y)  # radius at y
+        theta = phi*i  # golden angle increment
+        x = math.cos(theta) * radius
+        z = math.sin(theta) * radius
+        points.append((x, y, z))
+    return np.array(points)
