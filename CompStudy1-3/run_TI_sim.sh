de4272edd0f5009efa5850f9ed339b5696e6802a
chaitanyagoswami <chaitanya.goswami96@gmail.com>
2024-07-19 10:26:09 -0400

    Delete CompStudy1-3 directory

--- CompStudy1-3/run_TI_sim.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-python3 TI_sim_1d.py 2000 2020 500 1200
-python3 TI_sim.py 2000 2020 500 1200
-python3 plot_TI_sim.py
--- CompStudy1-3/run_non-invasive_sim.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-python3 non-invasive_sim.py 0 100 1000
-python3 non-invasive_sim.py 1 100 1000
-python3 non-invasive_sim.py 2 100 1000
-python3 plot_summary_non-invasive.py 0
-python3 plot_summary_non-invasive.py 1
-python3 plot_summary_non-invasive.py 2
