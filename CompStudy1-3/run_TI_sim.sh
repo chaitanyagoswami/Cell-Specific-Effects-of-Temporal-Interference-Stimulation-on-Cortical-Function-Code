b614c7586f4c481a9ea8d324713dfe36ce306646
chaitanyagoswami <chaitanya.goswami96@gmail.com>
2024-07-20 18:00:49 -0400

    Update run_TI_sim.sh

--- CompStudy1-3/run_TI_sim.sh
+++ CompStudy1-3/run_TI_sim.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-python3 TI_sim_1d.py 2000 2020 500 1200
-python3 TI_sim.py 2000 2020 500 1200
+python3 TI_sim_1d.py 2000 2020 500 1200 
+python3 TI_sim.py 2000 2020 500 1200 0 # Target
+python3 TI_sim.py 2000 2020 500 1200 1 # Non-Target
 python3 plot_TI_sim.py
